ffcd836caf1367f60c829f7381a1c240322da6d4
Kadacheahmedrami <a_kadache@estin.dz>
2024-12-05 21:52:20 +0100

    adding title

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Focal from "../components/Focal"
 import Focal1 from "../components/Focal1"
 import Band from "../components/Band"
+import Title from "../components/Title"
 
 
 export default function Home() {
@@ -14,7 +15,7 @@ export default function Home() {
    <Band></Band>
  
    <Focal1></Focal1>
-
+    <Title></Title>
     </>
   );
 }
---
+++ components/Title.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+function Title() {
+  return (
+  <>
+  <div className=' text-center  text-[40px] lg:text-[60px] font-bold  w-full'>specialities</div>
+  <div className='  text-center text-[20px] lg:text-[30px] opacity-60 mx-auto  w-[90%] lg:w-[40%]  '>Whatever speciality you need a doctor for , we carry lists of doctors to help out with that </div>
+  </>
+  )
+}
+
+export default Title
