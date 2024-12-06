5089f67df8e7118bbc5e0a27db2dd55a286a3aca
Kadacheahmedrami <a_kadache@estin.dz>
2024-12-06 14:26:49 +0100

    finishing the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,36 +3,195 @@ import Image from "next/image";
 
 function Footer() {
   return (
-    <div className="relative">
+    <div className="relative ">
       {/* Image */}
  
-      <Image
-        src={"/webp/heal.webp"}
-        height={550}
-        width={550}
-        alt="heal"
-        className="rounded-full mb-[-27%] ml-auto mr-[7%]   relative z-10"
-      />
-   <div className='h-[650px] w-[650px] rounded-full mb-[-32%] bottom-[100px]   ml-auto mr-[5%]  bg-[#F2F9FF] relative z-1'>
-        
-        </div>
-        <div className='h-[600px] w-[600px] rounded-full mb-[-32%] bottom-[100px]   ml-auto mr-[6%]  bg-[#E6F3FF] relative z-1'>
-        
-        </div>
+    
      
       {/* SVG */}
+
+      <Image
+        src={"/webp/heal.webp"}
+        height={500}
+        width={500}
+        alt="heal"
+        className="rounded-full right-[5%]  top-[-10px]   absolute hidden lg:block z-10"
+      />
+   <div className='h-[650px] w-[650px] rounded-full absolute hidden lg:block right-[1%] top-[-80px]  bg-[#F2F9FF]  z-1'>
+        
+        </div>
+        <div className='h-[600px] w-[600px] rounded-full absolute hidden lg:block  right-[2.5%]  top-[-30px]  bg-[#E6F3FF]  z-1'>
+        
+        </div>
       <svg
-        viewBox="0 0 1442 638"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute z-0 w-full h-auto mt-[150px]"
-      >
-        <path
-          d="M1 637H1441V61.0175C1201 21.0058 961 1 721 1C481 1 241 21.0058 1 61.0175V637Z"
-          fill="#0081FE"
-          stroke="#0081FE"
-        />
-      </svg>
+  viewBox="0 0 1442 638"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  className="absolute z-0 w-full h-auto mt-[150px] "
+>
+  <path
+    d="M1 637H1441V61.0175C1201 21.0058 961 1 721 1C481 1 241 21.0058 1 61.0175V637Z"
+    fill="#0081FE"
+    stroke="#0081FE"
+  />
+
+  <foreignObject x="0" y="0" width="1442" height="638">
+    <div>
+    
+
+
+
+      <div className="text-[100px] ml-[5%] mt-[7%] text-white">Ready to get started</div>
+
+      <div className="flex flex-col md:flex-row gap-7 mt-[0%] md:mt-[17%] relative z-1 justify-center items-center">
+      <div className="px-4 w-[30%] md:w-[280px]  mx-auto md:ml-[5%] bg-white relative gap-4 z-1 h-[100px] rounded-full justify-center items-center flex flex-row group hover:shadow-[0_0_20px_5px_rgba(255,255,255,0.7)]  cursor-pointer transition-all duration-300">
+  <img
+    src="/svg/apple.svg"
+    height={60}
+    width={60}
+    alt="apple"
+  />
+  <div className="flex flex-col">
+    <div className="text-[20px]">Download on the</div>
+    <div className="text-[25px] font-bold">APP store</div>
+  </div>
+</div>
+
+<div className="px-4 w-[30%] md:w-[280px] mx-auto md:mr-auto bg-black relative gap-4 z-1 h-[100px] rounded-full justify-center items-center flex flex-row group  hover:shadow-[0_0_20px_5px_rgba(0,0,0,0.7)] cursor-pointer transition-all ease duration-300">
+  <img
+    src="/svg/play.svg"
+    height={60}
+    width={60}
+    alt="play"
+  />
+  <div className="flex flex-col">
+    <div className="text-[20px] text-white">Download on the</div>
+    <div className="text-[25px] text-white font-bold">PLAY store</div>
+  </div>
+</div>
+
+
+        <div className="text-[21px] w-max text-white mx-auto md:mr-[5%]">
+          © 2019 APCOM. All Rights Reserved
+        </div>
+        <div className="flex gap-4 mx-auto md:mr-[5%]">
+  <a
+    href="https://www.facebook.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="transition-transform hover:scale-110 hover:filter hover:drop-shadow-[0_0_30px_rgba(255,255,255,1)]"
+  >
+    <img
+      src="/svg/Facebook.svg"
+      height={60}
+      width={60}
+      alt="Facebook"
+    />
+  </a>
+  <a
+    href="https://www.linkedin.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="transition-transform hover:scale-110 hover:filter hover:drop-shadow-[0_0_30px_rgba(255,255,255,1)]"
+  >
+    <img
+      src="/svg/Linkedin.svg"
+      height={60}
+      width={60}
+      alt="Linkedin"
+    />
+  </a>
+  <a
+    href="https://www.twitter.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    className=" hover:scale-110 ease transition-all 0.3s ease  hover:filter hover:drop-shadow-[0_0_5px_rgba(255,255,255,1)]"
+  >
+    <img
+      src="/svg/Twitter.svg"
+      height={60}
+      width={60}
+      alt="Twitter"
+      className="mr-[5%]"
+    />
+  </a>
+</div>
+
+      
+      </div>
+    </div>
+  </foreignObject>
+</svg>
+
+      {/* <div className='text-[120px] ml-[5%]'>Ready to get started</div>
+
+
+      <div className='flex flex-row gap-7 relative z-1  justify-center items-center '>
+
+      <div className=' px-4 w-[280px] ml-[5%] bg-white relative gap-4 z-1 h-[100px] rounded-full  justify-center items-center flex flex-row'>
+      <Image
+        src={"/svg/apple.svg"}
+        height={60}
+        width={60}
+        alt="apple"
+      
+      />
+      <div className='flex flex-col '>
+        <div className='text-[20px]'>Download on the </div>
+        <div className='text-[25px] font-bold'>APP store</div>
+      </div>
+      </div>
+
+      <div className=' px-4 w-[280px] mr-auto  bg-black relative gap-4 z-1 h-[100px] rounded-full  justify-center items-center flex flex-row'>
+      <Image
+        src={"/svg/play.svg"}
+        height={60}
+        width={60}
+        alt="play"
+      
+      />
+      <div className='flex flex-col '>
+        <div className='text-[20px] text-white'>Download on the </div>
+        <div className='text-[25px] text-white font-bold'>APP store</div>
+      </div>
+      </div>
+
+      <div className='text-[25px] text-white mr-[5%]'>
+      © 2019 APCOM. All Rights Reserved
+      </div>
+
+      <Image
+        src={"/svg/Facebook.svg"}
+        height={60}
+        width={60}
+        alt="Facebook"
+      
+      />
+         <Image
+        src={"/svg/Linkedin.svg"}
+        height={60}
+        width={60}
+        alt="Linkedin"
+      
+      />
+
+
+      <Image
+        src={"/svg/Twitter.svg"}
+        height={60}
+        width={60}
+        alt="Twitter"
+        className='mr-[5%]'
+      />
+
+
+
+
+      </div> */}
+    
+
+
+      
     </div>
   );
 }
