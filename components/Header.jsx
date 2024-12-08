4ca5061c32cb65fd1661c8394a068f24667e75a5
Kadacheahmedrami <a_kadache@estin.dz>
2024-12-08 10:57:49 +0100

    fixing deployment stuff

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,6 @@ const Header = () => {
         {/* Logo - Preloaded image for faster load */}
         <div   className="gap-2 mr-auto flex justify-center items-center text-[30px] font-bold"> 
         <Image 
-      
           src="/svg/logo.svg" 
           alt="logo" 
           width={60} 
@@ -54,14 +53,14 @@ const Header = () => {
 
         {/* Desktop Buttons */}
         <div className="hidden flex-row gap-6 lg:flex lg:items-center">
-  <a href="/signup">
+  <a href="/pages/auth/signup">
     <button className="relative h-12 w-40 overflow-hidden border border-[#1376F8] text-[#1376F8] shadow-2xl transition-all   rounded-[10px] duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-[#1376F8] before:duration-300 before:ease-out hover:text-white hover:shadow-[0_4px_20px_#1376F8] hover:before:h-40 hover:before:w-40 hover:before:opacity-100">
       <span className="relative z-10">Sign Up</span>
     </button>
   </a>
-  <a href="/login">
+  <a href="/pages/auth/login">
     <button className="relative h-12 w-40 overflow-hidden border border-[#1376F8] text-white hover:text-[#1376F8] rounded-[10px]  shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm bg-[#1376F8]  before:bg-white before:duration-300 before:ease-out  hover:shadow-[0_4px_20px_#1376F8] hover:before:h-40 hover:before:w-40 hover:before:opacity-100">
-      <span className="relative z-10">Sign In</span>
+      <span className="relative z-10">Log In</span>
     </button>
   </a>
 </div>
