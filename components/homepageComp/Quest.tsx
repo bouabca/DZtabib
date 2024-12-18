18e594f5c223aedad51b70a38691c3dcb02d9214
Kadacheahmedrami <a_kadache@estin.dz>
2024-12-18 14:45:14 +0100

    fixing question saction

--- components/homepageComp/Quest.tsx
+++ components/homepageComp/Quest.tsx
@@ -7,15 +7,15 @@ interface QuestProps {
 const Quest: React.FC<QuestProps> = ({ quest }) => {
   return (
     <div
-      className=" z-10 relative w-[90%] my-10 md:w-[800px] p-5 mx-auto rounded-lg cursor-pointer flex flex-col justify-start items-start gap-3 
-        bg-white shadow-md overflow-hidden transition-all duration-300 ease-in-out 
+      className=" relative w-[90%] my-10 md:w-[800px] p-5 mx-auto rounded-lg cursor-pointer flex flex-col justify-start items-start gap-3 
+        bg-white shadow-md overflow-visibile transition-all duration-300 ease-in-out 
         hover:scale-105 hover:shadow-[0_4px_20px_#1376F8] hover:text-white
         before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 
         before:rounded-lg before:bg-[#1376F8] before:transition-all before:duration-300 before:ease-out 
         hover:before:h-full hover:before:w-full"
     >
-      <li className="relative z-10 text-[22px] font-semibold">{quest}</li>
-      <div className="relative z-10 w-[98%] h-[2px] bg-black opacity-40 rounded-full"></div>
+      <li className="relative text-[22px] font-semibold">{quest}</li>
+      <div className="relative w-[98%] h-[2px] bg-black opacity-40 rounded-full"></div>
     </div>
   );
 };
--- components/nav.tsx
+++ components/nav.tsx
@@ -17,7 +17,7 @@ const Nav = () => {
             const isActive = pathname.includes(tab.toLowerCase());
 
             return (  
-            <Link key={tab} href={`/pages/dash/${tab.toLowerCase()}`} passHref className='w-[90%] '>
+            <Link key={tab} href={`/pages/dashDoc/${tab.toLowerCase()}`} passHref className='w-[90%] '>
                 <div
                 className={`text-[16px] lg:text-[24px]   rounded-[12px] p-[7px] px-[25px] w-[320px]  md:w-[100%] h-auto  cursor-pointer transition-all duration-300 ${
                     isActive
