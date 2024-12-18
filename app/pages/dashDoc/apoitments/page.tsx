e8b995ed9d499e2a020ab610faa13c74f1cc632a
Kadacheahmedrami <a_kadache@estin.dz>
2024-12-18 19:49:22 +0100

    creating new component

--- app/pages/dashDoc/apoitments/page.tsx
+++ app/pages/dashDoc/apoitments/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from "react";
 import AppointmentList from "../../../../components/docdashComp/list";
-
+import Dashhead from "../../../../components/docdashComp/dashHead"
 const Apoitments = () => {
   const [search, setSearch] = useState(""); // State for search input
 
@@ -28,14 +28,8 @@ const Apoitments = () => {
         onChange={(e) => setSearch(e.target.value)} // Update search input on change
       />
       <div className="p-4 m-4 rounded-[10px] overflow-visible  w-[1200px] md:w-[98%] mx-auto    bg-white">
-        <div className="w-[99.9%]  text-[24px] flex flex-row justify-between  px-8  items-center mx-auto bg-slate-200 py-4 ">
-          <div className="w-[150px] text-start border-solid border-2 border-black">Date</div>
-          <div className="w-[200px] text-start border-solid border-2 border-black">Patient</div>
-          <div className="w-[180px] text-start border-solid border-2 border-black">apoitment type</div>
-          <div className="w-[250px] text-start border-solid border-2 border-black">Numero</div>
-          <div className="w-[250px] text-start border-solid border-2 border-black">Gmail</div>
-          <div className="w-[200px] text-start border-solid border-2 border-black">Start Meet</div>
-        </div>
+        
+        <Dashhead></Dashhead>
 
         {/* Pass the filtered appointments to AppointmentList */}
         <AppointmentList appointments={filteredAppointments} />
---
+++ components/docdashComp/dashHead.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+const Dashhead = () => {
+      
+
+
+    return (  
+        <>
+    <div className="w-[99.9%]  text-[24px] flex flex-row justify-between  px-8  items-center mx-auto bg-slate-200 py-4 ">
+          <div className="w-[150px] text-start border-solid border-2 border-black">Date</div>
+          <div className="w-[200px] text-start border-solid border-2 border-black">Patient</div>
+          <div className="w-[180px] text-start border-solid border-2 border-black">apoitment type</div>
+          <div className="w-[250px] text-start border-solid border-2 border-black">Numero</div>
+          <div className="w-[250px] text-start border-solid border-2 border-black">Gmail</div>
+          <div className="w-[200px] text-start border-solid border-2 border-black">Start Meet</div>
+        </div>
+    
+        </>
+     
+    );
+};
+
+export default Dashhead;
