bbf089a1f80eafa08702e1529de07db98d81c8ad
Kadacheahmedrami <a_kadache@estin.dz>
2024-12-18 19:44:07 +0100

    defrent text

--- app/pages/dashDoc/apoitments/page.tsx
+++ app/pages/dashDoc/apoitments/page.tsx
@@ -27,11 +27,11 @@ const Apoitments = () => {
         value={search}
         onChange={(e) => setSearch(e.target.value)} // Update search input on change
       />
-      <div className="p-4 m-4 rounded-[10px] overflow-visible  w-[1200px] md:w-[98%] mx-auto bg-white">
-        <div className="w-[99.9%] text-[24px] flex flex-row justify-between px-8 items-center mx-auto bg-slate-300 py-4 ">
+      <div className="p-4 m-4 rounded-[10px] overflow-visible  w-[1200px] md:w-[98%] mx-auto    bg-white">
+        <div className="w-[99.9%]  text-[24px] flex flex-row justify-between  px-8  items-center mx-auto bg-slate-200 py-4 ">
           <div className="w-[150px] text-start border-solid border-2 border-black">Date</div>
           <div className="w-[200px] text-start border-solid border-2 border-black">Patient</div>
-          <div className="w-[180px] text-start border-solid border-2 border-black">Appointment Type</div>
+          <div className="w-[180px] text-start border-solid border-2 border-black">apoitment type</div>
           <div className="w-[250px] text-start border-solid border-2 border-black">Numero</div>
           <div className="w-[250px] text-start border-solid border-2 border-black">Gmail</div>
           <div className="w-[200px] text-start border-solid border-2 border-black">Start Meet</div>
