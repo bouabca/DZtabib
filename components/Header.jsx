6616770e57d09c3559348250367e39f8f305af0a
Kadacheahmedrami <a_kadache@estin.dz>
2024-12-21 00:05:15 +0100

    trying to enhance performance

--- components/Header.jsx
+++ components/Header.jsx
@@ -53,16 +53,16 @@ const Header = () => {
 
         {/* Desktop Buttons */}
         <div className="hidden flex-row gap-6 lg:flex lg:items-center">
-  <a href="/pages/auth/signup">
+  <Link href="/pages/auth/signup">
     <button className="relative h-12 w-40 overflow-hidden border border-[#1376F8] text-[#1376F8] shadow-2xl transition-all   rounded-[10px] duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-[#1376F8] before:duration-300 before:ease-out hover:text-white hover:shadow-[0_4px_20px_#1376F8] hover:before:h-40 hover:before:w-40 hover:before:opacity-100">
-      <span className="relative z-10">Sign Up</span>
+      <span className="relative z-1">Sign Up</span>
     </button>
-  </a>
-  <a href="/pages/auth/login">
+  </Link>
+  <Link href="/pages/auth/login">
     <button className="relative h-12 w-40 overflow-hidden border border-[#1376F8] text-white hover:text-[#1376F8] rounded-[10px]  shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm bg-[#1376F8]  before:bg-white before:duration-300 before:ease-out  hover:shadow-[0_4px_20px_#1376F8] hover:before:h-40 hover:before:w-40 hover:before:opacity-100">
-      <span className="relative z-10">Log In</span>
+      <span className="relative z-1">Log In</span>
     </button>
-  </a>
+  </Link>
 </div>
 
 
