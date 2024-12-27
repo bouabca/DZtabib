acc7dd68928156356fe9595908de575d19be508a
You <you@example.cm>
2024-12-27 23:56:29 +0100

    test

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -11,7 +11,7 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [token, setToken] = useState<string | null>("weee youceff ");
+  const [token, setToken] = useState<string | null>(null);
   const [userdata, setuserdata] = useState<Object | null>({
     data: {
       email: "exemple@gmail.com" ,
