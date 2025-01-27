d6f7d80e8aa5b6c1ea3bec4f53d9270f439cf073
Kadacheahmedrami <a_kadache@estin.dz>
2025-01-27 17:46:21 +0100

    dep

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,22 +1,25 @@
 import axios from "axios";
 
 
-const fetchDataWithCredentials = async (): Promise<any> => {
-    try {
-      const response = await axios.get('https://dz-tabib-backend.onrender.com/cookie', {
-        withCredentials: true, // Include credentials (cookies, authentication headers)
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      return response.data;
-      
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      throw error;
-    }
-  };
+interface CookieResponse {
+  [key: string]: string;  // You can adjust this based on the actual structure of the cookies
+}
+
+const fetchDataWithCredentials = async (): Promise<CookieResponse> => {
+  try {
+    const response = await axios.get('https://dz-tabib-backend.onrender.com/cookie', {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response.data; // Now TypeScript knows this is of type CookieResponse
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
 
 
 export default function Home() {
